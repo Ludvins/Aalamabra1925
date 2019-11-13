4db6af97c8efcac38c009d69abc1b666ca89393c
Ludvins <theludvins@gmail.com>
2019-11-13 22:41:00 +0100

    Game Fragment

--- app/src/main/java/com/example/aaalamabra1925/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/aaalamabra1925/ui/game/GameFragment.kt
@@ -1,24 +1,38 @@
 package com.example.aaalamabra1925.ui.game
 
-import android.content.Context
-import android.net.Uri
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.TextView
 import com.example.aaalamabra1925.R
 
 
-
 class GameFragment : Fragment() {
+
+    private var questions = listOf(
+        Pair("This one is True", true),
+        Pair("This one is false", false),
+        Pair("This one is also True", true)
+
+
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_game, container, false)
+        quickNoteDialog()
+        val remainingQuestions = questions.shuffled()
+
+        val qView = root.findViewById<TextView>(R.id.question_view)
+
+        remainingQuestions.forEach {
+            qView.text = it.first
+        }
 
 
 
@@ -26,4 +40,16 @@ class GameFragment : Fragment() {
     }
 
 
+    private fun quickNoteDialog() {
+        val dialogBuilder = AlertDialog.Builder(context)
+        val dialogView = View.inflate(context, R.layout.dialog_game_explanation, null)
+        dialogBuilder.setView(dialogView)
+
+        dialogBuilder.setTitle("New Task")
+        dialogBuilder.setPositiveButton("Okay") { _, _ ->
+            //pass
+        }
+        val b = dialogBuilder.create()
+        b.show()
+    }
 }
